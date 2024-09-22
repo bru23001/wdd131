8a489389a37f1c2444edf13efaaf0625422f19bc
bru23001 <bru23001@byui.edu>
2024-09-21 23:20:12 -0500

    assigment home page

---
+++ scripts/getdates.js
@@ -0,0 +1,6 @@
+
+let currentYear = new Date().getFullYear();
+
+document.getElementById("current-year").textContent = currentYear;
+
+document.getElementById("last-modified").textContent = document.lastModified;
